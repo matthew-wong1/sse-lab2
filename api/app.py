bebd2826c147384b88b462ac5d42cd646886b07e
Matthew Wong <klw19@imperial.ac.uk>
2023-10-16 16:32:47 +0100

    add bootstrap5 styling to form

--- api/app.py
+++ api/app.py
@@ -2,11 +2,20 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
 	return render_template("index.html")
 
 @app.route("/submit", methods=["POST"])
 def submit():
 	input_name = request.form.get("name")
 	input_age = request.form.get("age")
+	input
 	return render_template("hello.html", name=input_name, age=input_age)
+
+#Use cookies, dynamic forms. if c ookie value not empty, display form 
+#Name, age (check that it is a number), UK number, make sure is ic.ac.uk email
+#Your IP address is... 
+#MAc address 
+#(Maybe) what machine you are on 
+#port number 
+

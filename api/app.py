e3be15d3f434945cca521447f1e8bbdf50ef9ea8
Matthew Wong <klw19@imperial.ac.uk>
2023-10-16 16:59:57 +0100

    update server routing

--- api/app.py
+++ api/app.py
@@ -7,10 +7,15 @@ def index():
 
 @app.route("/submit", methods=["POST"])
 def submit():
+	# data from form 
 	input_name = request.form.get("name")
 	input_age = request.form.get("age")
-	input
-	return render_template("hello.html", name=input_name, age=input_age)
+	input_tel = request.form.get("tel")
+	input_email = request.form.get("email") 
+
+	# data from GET quest 
+	ip_address = request.remote_addr
+	return render_template("user_data.html", name=input_name, age=input_age, ip_address=ip_address)
 
 #Use cookies, dynamic forms. if c ookie value not empty, display form 
 #Name, age (check that it is a number), UK number, make sure is ic.ac.uk email

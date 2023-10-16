79077f48afc049d277415a6cc8afa53944c29437
Matthew Wong <klw19@imperial.ac.uk>
2023-10-16 18:10:43 +0100

    add cookie storage of user data

--- api/app.py
+++ api/app.py
@@ -1,21 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 app = Flask(__name__)
+import sys
 
 @app.route("/")
 def index():
+	# Return user data page if cookies are set 
+	user_data = {}
+	if len(request.cookies) != 0:
+		for cookie in request.cookies:
+			user_data[cookie] = request.cookies.get(cookie)
+
+		return render_template("user_data.html", user_data=user_data)
+
 	return render_template("index.html")
 
-@app.route("/submit", methods=["POST"])
+@app.route("/your_data", methods=["POST"])
 def submit():
 	# data from form 
-	input_name = request.form.get("name")
-	input_age = request.form.get("age")
-	input_tel = request.form.get("tel")
-	input_email = request.form.get("email") 
+	user_data = {} 
 
-	# data from GET quest 
-	ip_address = request.remote_addr
-	return render_template("user_data.html", name=input_name, age=input_age, ip_address=ip_address)
+	user_data["name"] = request.form.get("name")
+	user_data["age"] = request.form.get("age")
+	user_data["tel"] = request.form.get("tel")
+	user_data["email"] = request.form.get("email") 
+
+	# data from request headers 
+	user_agent = request.user_agent
+	user_data["ip_address"] = request.remote_addr
+	user_data["platform"] = user_agent.platform.capitalize()
+	user_data["browser"] = user_agent.browser.capitalize()
+
+	response = make_response(render_template("user_data.html", user_data=user_data))
+
+
+	# set cookies for all inputted data 
+	for key, value in user_data.items():
+		response.set_cookie(key, value)
+
+	return response
 
 #Use cookies, dynamic forms. if c ookie value not empty, display form 
 #Name, age (check that it is a number), UK number, make sure is ic.ac.uk email

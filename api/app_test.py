c56d638074f0da62e453576b384936ebf86fddc8
Matthew Wong <klw19@imperial.ac.uk>
2023-10-23 16:56:58 +0100

    add test cases for functions used in app.py

--- api/app_test.py
+++ api/app_test.py
@@ -1,4 +1,5 @@
-from app import process_query
+from app import process_query, check_name, check_age, check_tel, check_email
+import pytest
 
 
 def test_knows_about_dinosaurs():
@@ -8,3 +9,35 @@ def test_knows_about_dinosaurs():
 
 def test_does_not_know_about_asteroids():
     assert process_query("asteroids") == "Unknown"
+
+
+@pytest.mark.parametrize("test_input,expected",
+                         [("Matthew", True),
+                          ("M@tthew", False),
+                          ("12345", False)])
+def test_check_name(test_input, expected):
+    assert check_name(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected",
+                         [("12", True),
+                          ("Matthew", False),
+                          ("12!", False)])
+def test_check_age(test_input, expected):
+    assert check_age(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected",
+                         [("0123456789", True),
+                          ("123456789", False),
+                          ("+441234567", False)])
+def test_check_tel(test_input, expected):
+    assert check_tel(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected",
+                         [("test@ic.ac.uk", True),
+                          ("test@gmail.com", False),
+                          ("test@ic.ac.uk@gmail.com", False)])
+def test_check_email(test_input, expected):
+    assert check_email(test_input) == expected
